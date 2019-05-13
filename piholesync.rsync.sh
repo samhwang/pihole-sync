771c4c1893d2a6e4582ca6316408d1bc1e64ba84
Sam Huynh <samhwang21121994@gmail.com>
2019-05-13 23:21:29 +1000

    Add pihole sync script
    Currently this is version 2.1 by /u/jvinch76, /u/LandlordTiberius
    and /u/ShawnEngland (github: @icemansid)

---
+++ piholesync.rsync.sh
@@ -0,0 +1,36 @@
+#VARS
+FILES=(black.list blacklist.txt regex.list whitelist.txt lan.list) #list of files you want to sync
+PIHOLEDIR=/etc/pihole #working dir of pihole
+PIHOLE2=piholezero.local #IP of 2nd PiHole
+HAUSER=root #user of second pihole
+ 
+#LOOP FOR FILE TRANSFER
+RESTART=0 # flag determine if service restart is needed
+for FILE in ${FILES[@]}
+do
+  if [[ -f $PIHOLEDIR/$FILE ]]; then
+  RSYNC_COMMAND=$(rsync -ai $PIHOLEDIR/$FILE $HAUSER@$PIHOLE2:$PIHOLEDIR)
+    if [[ -n "${RSYNC_COMMAND}" ]]; then
+      # rsync copied changes
+      RESTART=1 # restart flagged
+     # else
+       # no changes
+     fi
+  # else
+    # file does not exist, skipping
+  fi
+done
+ 
+FILE="adlists.list"
+RSYNC_COMMAND=$(rsync -ai $PIHOLEDIR/$FILE $HAUSER@$PIHOLE2:$PIHOLEDIR)
+if [[ -n "${RSYNC_COMMAND}" ]]; then
+  # rsync copied changes, update GRAVITY
+  ssh $HAUSER@$PIHOLE2 "sudo -S pihole -g"
+# else
+  # no changes
+fi
+ 
+if [ $RESTART == "1" ]; then
+  # INSTALL FILES AND RESTART pihole
+  ssh $HAUSER@$PIHOLE2 "sudo -S service pihole-FTL restart"
+fi
