5495f0ba70ee6a050debaebc28bbb11948adf19c
Sam Huynh <samhwang21121994@gmail.com>
2019-05-14 17:19:26 +1000

    Adjust the script
    - Remove the secondary pihole ip from the vars
    
    - Changing the user from root to pi
    
    - Add sudo rsync path to have root access over
    public key authentication and non-root user.

--- piholesync.rsync.sh
+++ piholesync.rsync.sh
@@ -1,15 +1,15 @@
 #VARS
 FILES=(black.list blacklist.txt regex.list whitelist.txt lan.list) #list of files you want to sync
 PIHOLEDIR=/etc/pihole #working dir of pihole
-PIHOLE2=piholezero.local #IP of 2nd PiHole
-HAUSER=root #user of second pihole
+PIHOLE2=[YOUR.OTHER.PIHOLE.IP] #IP of 2nd PiHole
+HAUSER=pi #user of second pihole
  
 #LOOP FOR FILE TRANSFER
 RESTART=0 # flag determine if service restart is needed
 for FILE in ${FILES[@]}
 do
   if [[ -f $PIHOLEDIR/$FILE ]]; then
-  RSYNC_COMMAND=$(rsync -ai $PIHOLEDIR/$FILE $HAUSER@$PIHOLE2:$PIHOLEDIR)
+  RSYNC_COMMAND=$(rsync -ai $PIHOLEDIR/$FILE $HAUSER@$PIHOLE2:$PIHOLEDIR --rsync-path="sudo rsync")
     if [[ -n "${RSYNC_COMMAND}" ]]; then
       # rsync copied changes
       RESTART=1 # restart flagged
